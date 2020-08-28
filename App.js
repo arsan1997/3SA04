24b8b11ca28f15ed7fac6a6c3b8de76caf1a51af
arsan1997 <system.call@outlook.com>
2020-08-29 02:47:36 +0700

    Router

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import Weather from './components/Weather';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ZipCodeScreen from './screen/ZipCodeScreen';
+import WeatherScreen from './screen/WeatherScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-    const doIt = () => {
-    console.log("Hello from console")
- }
-    return (
-      <View style={styles.container}>
-      <Weather zipCode="90110"/>
-      <StatusBar style="auto" />
-      </View>
- );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen} />
+        <Stack.Screen name="Weather" component={WeatherScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
- container: {
-     paddingTop: 25,
-     width: '100%',
-     height: '100%'
-     },
-});

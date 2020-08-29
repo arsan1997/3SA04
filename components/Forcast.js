25e1024e080ea95b04a41cf0e7d9596b8daa5312
arsan1997 <system.call@outlook.com>
2020-08-29 20:32:35 +0700

    css layout

--- components/Forcast.js
+++ components/Forcast.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Text, View,StyleSheet } from 'react-native';
+import { Text, View,StyleSheet, Image } from 'react-native';
+
+
 export default function Forcast(props) {
+    const check  = () => {
+        console.log(props.description)
+    }
+ 
     return (
         <View>
-        <Text style={styles.temp}>{props.main}</Text>
-         <Text style={styles.temp}>{props.description}</Text>
-        <View>
+        <View style={styles.box}>
+           <Text style={styles.temp} onPress={check}>{props.main}</Text>
+           <Text style={styles.temp}>{props.description}</Text>
             <Text style={styles.temp}>{props.temp}°C</Text>
-            
+            <Image source={require('../assets/cloud.png')} style={styles.imageShow}/>
         </View>
     </View>
     );
@@ -21,4 +27,15 @@ export default function Forcast(props) {
          textShadowColor: '#000',
 
     },
+    imageShow: {
+        marginTop: '20px',
+        width: '30px',
+        height: '20px'
+    },
+    box: {
+        display: "flex",
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 })
--- components/Weather.js
+++ components/Weather.js
@@ -60,9 +60,7 @@ const styles = StyleSheet.create({
 
      justifyContent: 'center',
 
-     alignItems: 'center',
-    
-     textAlign:'center'
+     alignItems: 'center'
      
      
  },
